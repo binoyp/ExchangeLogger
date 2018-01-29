4412efccd80a854c8e80900d15eba07296589980
Binoy Pilakkat <binoypilakkat@outlook.com>
2018-01-30 01:09:14 +0530

    Bokeh Plots of Data.ipynb implemented for db plotting

--- FPaths.py
+++ FPaths.py
@@ -1,5 +1,5 @@
 EXCEL_PATH = r"D:\AdMarenDrive\Admaren Shared\Arbitrage\Arbitrage_v1.xlsm"
-DBPATH = r"D:\AdMarenDrive\Admaren Shared\Arbitrage\cryptodata.bin"
+DBPATH = r"D:\AdMarenDrive\Admaren Shared\Arbitrage\exceldata.bin"
 RUN1LOG = "run1.log"
 RUN2LOG = "run2.log"
 BINANCELOG = 'binance.log'
--- logreader.py
+++ logreader.py
@@ -33,9 +33,9 @@ if __name__ == "__main__":
         colno = int(sys.argv[2])
 
     else:
-        k = 'BCH/BTC'
-        colno = 1
-    with open('cryptodata.bin', 'rb') as _f:
+        k = 'OMG/ETH'  # change here for various currencies
+        colno = 0 # choose column
+    with open(r"D:\AdMarenDrive\Admaren Shared\Arbitrage\exceldata.bin", 'rb') as _f:
         i = 0
         
         arrdata = []
@@ -46,15 +46,18 @@ if __name__ == "__main__":
                 curarr = np.array(d['data'])
                 xdata.append(d['ts'])
                 # print curarr[headers[k], :]
-                arrdata.append(curarr[headers[k], :])
+                try:
+                    arrdata.append(curarr[headers[k], :])
+                except Exception,e:
+                    print("Error %s with:\n\t%s"%(e,d))
 
             except EOFError:
                 break
         final_array = np.array(arrdata)
         xdata = np.array(xdata)
 
-        fx = xdata[np.abs(final_array[:, colno]) < 1e6]
-        fy = final_array[:, colno][np.abs(final_array[:, colno]) < 1e6]
+        fx = xdata[np.abs(final_array[:, colno]) < 1e5]
+        fy = final_array[:, colno][np.abs(final_array[:, colno]) < 1e5]
         plt.plot(fx, fy, 'r-o')
 
         plt.title(k + '- ' + titles[colno])
